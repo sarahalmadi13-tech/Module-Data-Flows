7a9d7e7bc58e391cda304082c8a2a71dbeb95579
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-09 22:59:08 +0200

    Format And Destructure The Object Inside The Loop

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+//print out the receipt for this order
+console.log("QTY     ITEM                TOTAL");
+//Looping with destructuring
+let grandTotal = 0;
+//object destructuring inside the loop
+for (let { itemName, quantity, unitPricePence } of order) {
+  //total is the total price for that line in euros (because we divide pence by 100).
+  let total = (quantity * unitPricePence) / 100;
+  //Formatting each line
+  //Adding colomn names and their width
+  console.log(
+    `${String(quantity).padEnd(7)}${itemName.padEnd(20)}${total.toFixed(2)}`
+  );
+
+  grandTotal += total;
+}
+
+console.log(`\nTotal: ${grandTotal.toFixed(2)}`);
