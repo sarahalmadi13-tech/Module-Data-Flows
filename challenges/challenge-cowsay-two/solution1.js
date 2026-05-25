3c7300f88c7674086b4927e8dbd33234743fbf50
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-25 09:51:57 +0200

    Refactor cowsay function to improve argument handling and speech bubble formatting

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -1,30 +1,45 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
 
 // 1. Accept arguments
+const saying = process.argv[2];
 
 // how will you accept arguments?
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = "----------";
+let bottomLine = "----------";
 
 // 3. Make a cow that takes a string
 
-function cowsay(saying) {
+function cowsay(text) {
+  if (!text) {
+    return "The cow refuses to speak without text!";
+  }
+  const bubble = ` ${topLine}
+< ${text} >
+ ${bottomLine}
+ `;
+  const cow = `
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+    `;
+  return bubble + cow;
+}
 // how will you make the speech bubble contain the text?
 
 // where will the cow picture go?
 
 // how will you account for the parameter being empty?
 
-}
-
 //4. Pipe argument into cowsay function and return a cow
 
 // how will you log this to the console?
+console.log(cowsay(saying));
