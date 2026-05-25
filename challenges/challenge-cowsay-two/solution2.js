dc9df0843e73005d3ac882526261110e2023b97c
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-25 10:33:05 +0200

    Implement cowsay CLI with readline for user input and speech bubble formatting

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,18 +1,41 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
 // 1. Make  a command line interface.
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // 2. Make supplies for our speech bubble
+const topLine = "--------------";
+const bottomLine = "--------------";
 
 // 3. Make a cow that takes a string
 
-const cow = (saying) => {
-    // how did you make the cow before?
-}
+const cow = (text) => {
+  const bubble = ` ${topLine}
+< ${text} >
+ ${bottomLine}
+ `;
+  const cow = `
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+    `;
+  return bubble + cow;
+};
 
-// 4. Use readline to get a string from the terminal 
+// 4. Use readline to get a string from the terminal
 // (with a prompt so it's clearer what we want)
+rl.question("What does the cow say? ", (answer) => {
+  console.log(cow(answer));
+  rl.close();
+});
