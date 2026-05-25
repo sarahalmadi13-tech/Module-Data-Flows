49a7f404a37cfdf86d8d9ba357ddacae2c45708b
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-25 23:21:32 +0200

    feat: implement sales function to calculate total car sales by make

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,14 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let totals = {};
+
+  for (let car of carsSold) {
+    if (!totals[car.make]) {
+      totals[car.make] = 0;
+    }
+    totals[car.make] += car.price;
+  }
+
+  return totals;
+}
 
 module.exports = sales;
