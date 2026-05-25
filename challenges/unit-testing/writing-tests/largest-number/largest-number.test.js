b2de8a87ebdf0acedc322eed6e83fde6895fdbd1
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-25 23:34:49 +0200

    test: enhance largest number tests with input and expected values

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -2,12 +2,25 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  let input = [3, 21, 88, 4, 36];
+  let expected = 88;
   // Act
+  let output = getLargestNumber(input);
   // Assert
+  expect(output).toBe(expected);
 });
 
-// example
-// input: [3, 21, 88, 4, 36];
-// expected: 88;
+test("does not modify the original array", function () {
+  // example
+  // input: [3, 21, 88, 4, 36];
+  // expected: 88;
 
-// also test that the original array hasn't changed
+  // also test that the original array hasn't changed
+  let input = [3, 21, 88, 4, 36];
+  let originalInput = [...input]; // Create a copy of the original array
+
+  // Act
+  getLargestNumber(input);
+  // Assert
+  expect(input).toEqual(originalInput);
+});
