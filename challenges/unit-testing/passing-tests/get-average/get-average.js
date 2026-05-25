a347aaf82491d15697d07630b2c4bbb658d0e9e0
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-25 23:29:49 +0200

    feat: implement average function to calculate the average of an array of numbers

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,18 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let sum = 0;
+  let count = 0;
+
+  for (let value of numbers) {
+    if (typeof value === "number") {
+      sum += value;
+      count++;
+    }
+  }
+
+  return sum / count;
+}
 
 module.exports = average;
